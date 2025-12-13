d315117155a5e21937ab81a3630ef420b5194d12
bcaceres19 <bacg20044@gmail.com>
2025-12-13 11:56:35 -0500

    General inquiry added

--- src/main/java/co/parameta/technical/test/commons/repository/SystemParameterRepository.java
+++ src/main/java/co/parameta/technical/test/commons/repository/SystemParameterRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SystemParameterRepository extends JpaRepository<SystemParameterEntity, Integer> {
+
+    SystemParameterEntity findByName(String name);
+
 }
